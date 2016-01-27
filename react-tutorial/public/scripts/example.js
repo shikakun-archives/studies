5adae66d7461db9b4369e914bdcad18cb0275b21
shikakun <k@u-n.jp>
2016-01-27 19:48:46 +0900

    Comment コンポーネントを書いた

--- react-tutorial/public/scripts/example.js
+++ react-tutorial/public/scripts/example.js
@@ -30,6 +30,19 @@ var CommentForm = React.createClass({
   }
 });
 
+var Comment = React.createClass({
+  render: function() {
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        {this.props.children}
+      </div>
+    );
+  }
+});
+
 ReactDOM.render(
   <CommentBox />,
   document.getElementById('content')
