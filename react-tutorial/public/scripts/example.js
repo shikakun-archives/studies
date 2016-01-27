06f974f8a055d4d8d1c665bbfe671a717d32e1ac
shikakun <k@u-n.jp>
2016-01-27 20:02:19 +0900

    コメントのデータをレンダリングした

--- react-tutorial/public/scripts/example.js
+++ react-tutorial/public/scripts/example.js
@@ -12,10 +12,16 @@ var CommentBox = React.createClass({
 
 var CommentList = React.createClass({
   render: function() {
+    var commentNodes = this.props.data.map(function (comment) {
+      return (
+        <Comment author={comment.author}>
+          {comment.text}
+        </Comment>
+      );
+    });
     return (
       <div className="commentList">
-        <Comment author="Pete Hunt">This is one comment</Comment>
-        <Comment author="Jordan Walke">This is *another* comment</Comment>
+        {commentNodes}
       </div>
     );
   }
