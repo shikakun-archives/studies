b60904030f04db06499b10884b30f1e4108fc2cd
shikakun <k@u-n.jp>
2016-01-27 19:35:29 +0900

    CommentList と CommentForm コンポーネントを書いた

--- react-tutorial/public/scripts/example.js
+++ react-tutorial/public/scripts/example.js
@@ -11,3 +11,23 @@ ReactDOM.render(
   <CommentBox />,
   document.getElementById('content')
 );
+
+var CommentList = React.createClass({
+  render: function() {
+    return (
+      <div className="commentList">
+        Hello, world! I am a CommentList.
+      </div>
+    );
+  }
+});
+
+var CommentForm = React.createClass({
+  render: function() {
+    return (
+      <div className="commentForm">
+        Hello, world! I am a CommentForm.
+      </div>
+    );
+  }
+});
