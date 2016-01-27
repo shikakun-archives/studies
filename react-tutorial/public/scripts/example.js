8da68e5fe8573283076373723c3a7c73ab49859d
shikakun <k@u-n.jp>
2016-01-27 19:59:37 +0900

    コメントのデータが CommentList で扱えるようにした

--- react-tutorial/public/scripts/example.js
+++ react-tutorial/public/scripts/example.js
@@ -3,7 +3,7 @@ var CommentBox = React.createClass({
     return (
       <div className="commentBox">
         <h1>Comments</h1>
-        <CommentList />
+        <CommentList data={this.props.data} />
         <CommentForm />
       </div>
     );
@@ -50,6 +50,6 @@ var data = [
 ];
 
 ReactDOM.render(
-  <CommentBox />,
+  <CommentBox data={data} />,
   document.getElementById('content')
 );
